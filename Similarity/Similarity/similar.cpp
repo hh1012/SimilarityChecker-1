6039f4466b093c87d7b4b491f45e80d8f847e5d5
hoon.hur <hurhoon1012@gmail.com>
2025-07-23 15:51:38 +0900

    [feature] Score for partial score

--- Similarity/Similarity/similar.cpp
+++ Similarity/Similarity/similar.cpp
@@ -5,10 +5,28 @@ using namespace std;
 class LenChecker {
 public:
 	int getScore(const string& a, const string& b) {
-		if (a.length() == b.length()) return MAX_SCORE;
-		return 0;
+		if (a.length() == b.length()) {
+			return MAX_SCORE;
+		}
+		
+		int longerlen, shorterlen;
+		if (a.length() > b.length()) {
+			longerlen = a.length();
+			shorterlen = b.length();
+		}
+		else {
+			shorterlen = a.length();
+			longerlen = b.length();
+		}
+
+		if (longerlen >= shorterlen * 2) return 0.0;
+		
+		int ret = static_cast<int>((1.0 - (longerlen - shorterlen) / static_cast<double>(shorterlen)) * 60);
+
+		return ret;
+
 	}
 
 private:
-	const int MAX_SCORE = 60;
+	const int MAX_SCORE = 60.0;
 };
--- Similarity/Similarity/similar_test.cpp
+++ Similarity/Similarity/similar_test.cpp
@@ -4,8 +4,23 @@ using namespace testing;
 
 TEST(TC, TC1) {
 	LenChecker app;
-	int expected = app.getScore("ASD", "DSA");
-	EXPECT_EQ(60, expected);
+	int actual = app.getScore("ASD", "DSA");
+	int expected = 60;
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(TC, TC2) {
+	LenChecker app;
+	int actual = app.getScore("A", "BB");
+	int expected = 0;
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(TC, TC3) {
+	LenChecker app;
+	int actual = app.getScore("AAABB", "BAA");
+	int expected = 20;
+	EXPECT_EQ(expected, actual);
 }
 
 int main() {
