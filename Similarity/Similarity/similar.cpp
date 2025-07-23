dfefcf73b74100b5d0810581fb32d75ea3a4891b
hoon.hur <hurhoon1012@gmail.com>
2025-07-23 17:05:35 +0900

    final code

--- Similarity/Similarity/similar.cpp
+++ Similarity/Similarity/similar.cpp
@@ -1,6 +1,10 @@
 #include <string>
+#include <vector>
+#include <set>
 
 using std::string;
+using std::vector;
+using std::set;
 
 class LengthCheck {
 public:
@@ -30,4 +34,74 @@ private:
 
     const int LENGTH_MAX_POINT = 60;
     const int LENGTH_MIN_POINT = 0;
+};
+
+class CharChecker {
+public:
+    int getResult(string input1, string input2) {
+        if (isPerfectSameChars(input1, input2)) return CHAR_MAX_POINT;
+        if (isPerfectDifferentChars(input1, input2)) return CHAR_MIN_POINT;
+        return getPartialCharPoint(input1, input2);
+    }
+
+private:
+    vector<char> getBreakString(string str) {
+        std::set<char> st1;
+
+        for (char ch : str) {
+            st1.insert(ch);
+        }
+
+        vector<char> t1;
+        for (char ch : st1) {
+            t1.push_back(ch);
+        }
+
+        return t1;
+    }
+
+    bool isPerfectSameChars(const string& input1, const string& input2) {
+        int size1 = getBreakString(input1).size();
+        int size2 = getBreakString(input2).size();
+        if (size1 != size2) return false;
+
+        int sameCount = getSameCharCount(input1, input2);
+        if (size1 == sameCount) return true;
+        return false;
+    }
+
+    bool isPerfectDifferentChars(const string& input1, const string& input2) {
+        return (getSameCharCount(input1, input2) == 0);
+    }
+    int getSameCharCount(const string& input1, const string& input2) {
+        vector<char> input1Chars = getBreakString(input1);
+        vector<char> input2Chars = getBreakString(input2);
+
+        int result = 0;
+        for (char input1Char : input1Chars) {
+            for (char input2Char : input2Chars) {
+                if (input1Char == input2Char) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    int getTotalCount(string input1, string input2) {
+        vector<char> totalChars = getBreakString(input1 + input2);
+        return totalChars.size();
+    }
+
+    int getPartialCharPoint(string input1, string input2) {
+        int totalCount = getTotalCount(input1, input2);
+        int sameCount = getSameCharCount(input1, input2);
+
+        return CHAR_MAX_POINT * sameCount / totalCount;
+    }
+
+private:
+    const int CHAR_MAX_POINT = 40;
+    const int CHAR_MIN_POINT = 0;
 };
--- Similarity/Similarity/similar_test.cpp
+++ Similarity/Similarity/similar_test.cpp
@@ -42,6 +42,32 @@ TEST_F(LengthFixture, TC5) {
     diagLength(20, "SDF", "EWRWE");
 }
 
+class CharFixture : public Test {
+public:
+    CharChecker app;
+
+    void diagLength(int expectedPoint, string input1, string input2) {
+        int actual = app.getResult(input1, input2);
+        EXPECT_EQ(expectedPoint, actual);
+    }
+};
+
+TEST_F(CharFixture, TC1) {
+    diagLength(40, "ABC", "ABC");
+    diagLength(40, "T", "T");
+}
+
+TEST_F(CharFixture, TC2) {
+    diagLength(0, "ABC", "IUUI");
+    diagLength(0, "QQQ", "A");
+    diagLength(0, "B", "F");
+}
+
+TEST_F(CharFixture, TC3) {
+    diagLength(26, "ABC", "ABBBB");
+    diagLength(26, "ABBBB", "ABC");
+}
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();
